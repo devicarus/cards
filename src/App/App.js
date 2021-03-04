7cd1a4bd900a900cca6a6e6b63b7d1d36b7ca573
DEVICARUS <devicarus@omg.lol>
2021-03-04 12:24:03 +0100

    :sparkles: Added a route to Home

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Home from '../pages/Home'
 import Playground from '../pages/Playground'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/" exact component={Home} />
         <Route path="/playground" component={Playground} />
       </Switch>
     </Router>
