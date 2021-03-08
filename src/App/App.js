03179d439905fac83783695f1c19e365a9975ab4
DEVICARUS <devicarus@omg.lol>
2021-03-08 22:38:39 +0100

    :sparkles: Added routing to login for testing purposes

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
+import Login from '../pages/Login'
 import Playground from '../pages/Playground'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
         <Route path="/playground" component={Playground} />
       </Switch>
     </Router>
