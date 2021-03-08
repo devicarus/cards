3a1095e37f4bd2a86129c8f36b064a8a1f4be36c
DEVICARUS <devicarus@omg.lol>
2021-03-08 23:31:57 +0100

    :sparkles: Added NavLink wrapper

---
+++ src/components/NavLink.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+import { NavLink as ThemedLink } from 'theme-ui'
+import * as Icons from 'react-feather'
+
+function NavLink(props) {
+    const Icon = Icons[props.icon]
+
+    return (
+        <ThemedLink to={props.to} as={Link} p={2} sx={{ display: "flex", ...props.style }}>
+            { props.icon &&
+                <Icon style={{ marginRight: "5px" }} />
+            }
+            {props.label}
+        </ThemedLink>
+    )
+}
+
+export default NavLink
