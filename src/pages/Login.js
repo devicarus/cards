d3dd95783186e957d87a5229dd8ca65fc9518afd
DEVICARUS <devicarus@omg.lol>
2021-03-08 23:02:27 +0100

    :adhesive_bandage: Fixed Field margin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,7 +31,7 @@ function Login() {
                         <Badge bg="muted" mt="-5px">Closed</Badge>
                     </Flex>
                 </Flex>
-                <Field icon={Mail} placeholder="Email" mb={2} />
+                <Field icon={Mail} placeholder="Email" style={{ marginBottom: "5px" }} />
                 <Field icon={Lock} placeholder="Password" />
                 <Button sx={{ width: "100%" }} mt="30px">Sign In</Button>
             </Box>
