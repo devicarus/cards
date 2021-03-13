047c297a2cc71541b0c7f84b06cc13d27d26e72a
DEVICARUS <devicarus@omg.lol>
2021-03-13 19:26:08 +0100

    :sparkles: Added deck listing with dummy data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
 import React from 'react'
 
-import { Flex, Container, Box } from 'theme-ui'
+import { Flex, Container, Box, Image, Text } from 'theme-ui'
+import { Plus } from 'react-feather'
 
 import NavLink from '../components/wrappers/NavLink'
+
+const decks = [{
+    "name": "tristique tortor eu",
+    "image": "http://dummyimage.com/144x166.png/ff4444/ffffff"
+}, {
+    "name": "sit amet eros suspendisse",
+    "image": "http://dummyimage.com/144x166.png/cc0000/ffffff"
+}, {
+    "name": "et tempus semper",
+    "image": "http://dummyimage.com/144x166.bmp/cc0000/ffffff"
+}, {
+    "name": "leo odio",
+    "image": "http://dummyimage.com/144x166.jpg/cc0000/ffffff"
+}, {
+    "name": "velit eu est",
+    "image": "http://dummyimage.com/144x166.jpg/cc0000/ffffff"
+}, {
+    "name": "potenti cras in purus",
+    "image": "http://dummyimage.com/144x166.png/cc0000/ffffff"
+}, {
+    "name": "orci pede venenatis non",
+    "image": "http://dummyimage.com/144x166.bmp/5fa2dd/ffffff"
+}, {
+    "name": "sed augue",
+    "image": "http://dummyimage.com/144x166.png/dddddd/000000"
+}, {
+    "name": "cubilia curae",
+    "image": "http://dummyimage.com/144x166.png/5fa2dd/ffffff"
+}, {
+    "name": "nisi venenatis",
+    "image": "http://dummyimage.com/144x166.png/5fa2dd/ffffff"
+}]
 
 function Home() {
     return (<>
@@ -24,11 +57,44 @@ function Home() {
             <Box sx={{
                 boxShadow: "0 0 25px 0 rgba(0,0,0,.04)",
                 background: "white",
-                borderRadius: "10px",
+                borderRadius: [null, "10px"],
                 marginTop: "40px",
                 padding: "30px"
             }}>
-                
+                <Box sx={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fill, minmax(144px, 1fr))",
+                    gridGap: "20px",
+                    justifyItems: "center"
+                }}>
+                    {decks.map(deck =>
+                        <Box sx={{ width: "144px", cursor: "pointer" }}>
+                            <Box sx={{
+                                boxShadow: "0 2px 6px rgba(0,0,0,.1),0 6px 0 -1px #fff,0 7px 6px rgba(0,0,0,.1),0 11px 0 -1px #fff,0 12px 6px rgba(0,0,0,.1)",
+                                borderRadius: "18px",
+                                overflow: "hidden",
+                                height: "166px",
+                                marginBottom: "20px"
+                            }}>
+                                <Image src={deck.image} />
+                            </Box>
+                            <Text sx={{ fontFamily: "medium" }}>{deck.name}</Text>
+                        </Box>
+                    )}
+                    <Box sx={{ width: "144px", cursor: "pointer" }}>
+                        <Flex sx={{
+                            boxShadow: "0 2px 6px rgba(0,0,0,.1)",//"0 6px 0 -1px #fff,0 7px 6px rgba(0,0,0,.1),0 11px 0 -1px #fff,0 12px 6px rgba(0,0,0,.1)",
+                            borderRadius: "18px",
+                            overflow: "hidden",
+                            height: "166px",
+                            marginBottom: "20px",
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}>
+                            <Plus color="lightgray" size="30%" />
+                        </Flex>
+                    </Box>
+                </Box>
             </Box>
         </Container>
     </>)
