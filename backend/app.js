74d2a571b18650845a749d198605ccc75e1a0f41
DEVICARUS <devicarus@omg.lol>
2021-03-18 00:05:56 +0100

    :boom: (backend): Renamed the users endpoint to user

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 module.exports = app;
