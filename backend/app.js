fae4c877ca8dffb327800c9117acd8b68b107fcc
DEVICARUS <devicarus@omg.lol>
2021-04-03 15:23:34 +0200

    :sparkles: (backend): Added auth middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose')
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const bcrypt = require('bcrypt')
+
+const User = require('./models/User')
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/user');
 
 const app = express();
 
@@ -12,7 +22,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(passport.initialize())
+
+passport.use(
+    new JwtStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: "secret"
+    }, (jwt_payload, done) => {
+        console.log(jwt_payload)
+        User.findById(jwt_payload._id)
+            .then(user => {
+                if (user) {
+                    return done(null, user);
+                }
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
+    })
+);
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email'
+    }, (email, password, done) => {
+        User.findOne({ email }, function (err, user) {
+            if (err) return done(err);
+            if (!user) return done(null, false);
+            if (!bcrypt.compareSync(password, user.password)) return done(null, false);
+            return done(null, user);
+        });
+    })
+)
+
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
+
+mongoose.connect('mongodb://mongo:27017/cards')
 
 module.exports = app;
