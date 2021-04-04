a5f5317a770b3c0ac8df2dbf13df9f26e5586323
DEVICARUS <devicarus@omg.lol>
2021-04-05 01:30:18 +0200

    :recycle: (backend): Refactored the auth endpoints

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,49 +1,52 @@
-const express = require('express');
+const express = require('express')
 
 const passport = require('passport')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const router = express.Router();
-
 const User = require('../models/User')
 
-router.post('/register', (req, res, next) => {
-  User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      return res.status(400).send({ email: "Email already exists" });
-    } else {
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) throw err;
-        bcrypt.hash(req.body.password, salt, (err, hash) => {
-          if (err) throw err;
+const router = express.Router()
 
-          new User({
-            email: req.body.email,
-            password: hash
-          }).save()
-            .then(user => res.send(user))
-            .catch(err => console.log(err))
-        });
-      });
-    }
-  });
+router.post('/register', async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+
+  if (user) {
+    res.status(400).json({
+      message: "Email already exists"
+    })
+  } else {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(req.body.password, salt)
+
+    const newUser = await new User({
+      email: req.body.email,
+      password: hash
+    }).save()
+
+    res.json(newUser)
+  }
 })
 
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err || !user) {
-      return res.status(400).json({
+    if (err) {
+      res.status(500).json({
         message: 'Something is not right',
-        user: user
-      });
+        user
+      })
+    } else if (!user) {
+      res.status(400).json({
+        message: 'Wrong email or password',
+        user
+      })
+    } else {
+      res.json({
+        token: jwt.sign(user.toJSON(), 'secret'),
+        user
+      })
     }
-
-    res.send({
-      user,
-      token: jwt.sign(user.toJSON(), 'secret')
-    })
-  }) (req, res)
+  })(req, res)
 })
 
-module.exports = router;
+module.exports = router
