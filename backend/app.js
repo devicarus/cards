8293347af4dc3538a4029d5594d3ba9dd2ab6b40
DEVICARUS <devicarus@omg.lol>
2021-04-04 23:58:58 +0200

    :truck: (backend): Renamed router.js and passport.js

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose')
 
-const passport  = require('./passport')
-const router = require('./router')
+const setupPassport  = require('./setupPassport')
+const setupRoutes = require('./setupRoutes')
 
 const app = express();
 
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-passport(app)
-router(app)
+setupPassport(app)
+setupRoutes(app)
 
 mongoose.connect('mongodb://mongo:27017/cards')
 
