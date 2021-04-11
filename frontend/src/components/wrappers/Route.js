c10124c14c6d3dc6b89114070608a9fa1bdefa25
DEVICARUS <devicarus@omg.lol>
2021-04-12 00:59:31 +0200

    :sparkles: (frontend): Added a Route wrapper to support private routes

---
+++ frontend/src/components/wrappers/Route.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { Route as RouterRoute, Redirect } from 'react-router-dom'
+
+function Route(props) {
+    const token = useSelector(state => state.user.token)
+
+    return (
+        <>
+            { props.protected && !token &&
+                <Redirect to="/login" />
+            }
+            <RouterRoute {...props} />
+        </>
+    )
+}
+
+export default Route
