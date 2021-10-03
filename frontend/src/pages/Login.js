c31442d2ea7038c5088857ac4c2e6f0ec76e229b
DEVICARUS <devicarus@icloud.com>
2021-10-03 16:43:50 +0200

    :sparkles: (frontend): Added sign-up

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link, useParams, useHistory, useLocation } from 'react-router-dom'
 
 import { setToken } from '../store/reducers/user'
 
-import { Flex, Box, Button, Badge, Text } from 'theme-ui'
+import { Flex, Box, Button, Text, NavLink } from 'theme-ui'
 import Field from '../components/wrappers/Field'
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 function Login() {
     const dispatch = useDispatch()
+    const { mode } = useParams()
+    const history = useHistory()
+    const query = useQuery()
 
-    const [state, setState] = useState({ 
-        form: { 
-            email: "", 
-            password: "" 
-        },
-        error: "" 
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    })
+
+    const [error, setError] = useState({
+        fields: [],
+        text: ""
     })
 
     const token = useSelector(state => state.user.token)
@@ -34,17 +43,38 @@ function Login() {
                 break;
 
             case 400:
-                setState({ ...state, error: "Wrong email or password"})
+                setError({ fields: ["email", "password"], text: "Wrong email or password" })
                 break;
 
             default:
-                setState({ ...state, error: "Something went wrong, please try again later"})
+                setError({ fields: [], text: "Something went wrong, please try again later" })
+                break;
+        }
+    }
+
+    const register = async (form) => {
+        const response = await fetch("/api/auth/register", {
+            method: 'POST',
+            body: new URLSearchParams(form)
+        })
+
+        switch (response.status) {
+            case 200:
+                history.push("/signin?notice=registered")
+                break;
+
+            case 400:
+                setError({ fields: ["email"], text: "Email already used" })
+                break;
+
+            default:
+                setError({ fields: [], text: "Something went wrong, please try again later" })
                 break;
         }
     }
 
     return (<>
-        { token &&
+        {token &&
             <Redirect to='/' />
         }
 
@@ -64,24 +94,79 @@ function Login() {
                 p="30px"
             >
                 <Flex mb="30px">
-                    <Flex sx={{ flexGrow: 1, alignItems: "flex-start" }} >
-                        <Button variant="plain">Sign In</Button>
-                    </Flex>
-                    <Flex sx={{ flexGrow: 1, flexDirection: "column", alignItems: "center" }}>
-                        <Button variant="plainDisabled">Sign Up</Button>
-                        <Badge bg="muted" mt="-5px">Closed</Badge>
-                    </Flex>
+                    <NavLink
+                        sx={{
+                            flexGrow: 1,
+                            fontSize: 4,
+                            color: mode !== "in" ? "muted" : "text",
+                            "&:hover": {
+                                color: mode !== "in" ? "muted" : "text"
+                            },
+                            "&:focus": {
+                                color: mode !== "in" ? "muted" : "text"
+                            }
+                        }}
+                        as={Link}
+                        to="/signin"
+                        onClick={() => {
+                            setError({
+                                fields: [],
+                                text: ""
+                            })
+                        }}
+                    >
+                        <Text sx={{ textAlign: "center" }}>Sign In</Text>
+                    </NavLink>
+                    <NavLink
+                        sx={{
+                            flexGrow: 1,
+                            fontSize: 4,
+                            color: mode !== "up" ? "muted" : "text",
+                            "&:hover": {
+                                color: mode !== "up" ? "muted" : "text"
+                            },
+                            "&:focus": {
+                                color: mode !== "up" ? "muted" : "text"
+                            }
+                        }}
+                        as={Link}
+                        to="/signup"
+                        onClick={() => {
+                            setError({
+                                fields: [],
+                                text: ""
+                            })
+                        }}
+                    >
+                        <Text sx={{ textAlign: "center" }}>Sign Up</Text>
+                    </NavLink>
                 </Flex>
                 <Box as="form" onSubmit={e => {
-                    e.preventDefault()
-                    login(state.form)
-                }}>
-                    <Field icon="Mail" placeholder="Email" invalid={state.error} onChange={e => setState({ form: { ...state.form, email: e.target.value }})} containerStyle={{ marginBottom: "5px" }} />
-                    <Field icon="Lock" placeholder="Password" invalid={state.error} onChange={e => setState({ form: { ...state.form, password: e.target.value }})} type="password" />
-                    { state.error &&
-                        <Text mt={2} color="#FF3C38" sx={{ fontWeight: "bold", textAlign: "center" }}>{ state.error }</Text>
+                    if (mode === "in") {
+                        e.preventDefault()
+                        login(form)
+                    } else {
+                        e.preventDefault()
+                        register(form)
                     }
-                    <Button type="submit" sx={{ width: "100%" }} mt="30px">Sign In</Button>
+                }}>
+                    <Field icon="Mail" placeholder="Email" invalid={error.fields.includes("email")} onChange={e => setForm({ ...form, email: e.target.value })} containerStyle={{ marginBottom: "5px" }} />
+                    <Field icon="Lock" placeholder="Password" invalid={error.fields.includes("password")} onChange={e => setForm({ ...form, password: e.target.value })} type="password" />
+
+                    <Text mt={2} color="#FF3C38" sx={{ fontWeight: "bold", textAlign: "center" }}>{error.text}</Text>
+
+                    <Text mt={2} color="#16db65" sx={{ fontWeight: "bold", textAlign: "center" }}>{(() => {
+                        switch (query.get("notice")) {
+                            case "registered":
+                                return "You can now Sign In"
+
+                            default:
+                                return
+                        }
+                    })()}</Text>
+
+
+                    <Button type="submit" sx={{ width: "100%" }} mt="30px">Sign {mode}</Button>
                 </Box>
             </Box>
         </Flex>
