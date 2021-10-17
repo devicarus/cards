ea26dae8ab1f88a0ee2f57cc5b07804e89f60ce3
DEVICARUS <devicarus@icloud.com>
2021-10-17 18:06:09 +0200

    :lock: (backend): Successful login no longer returns the whole user object

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,8 +64,7 @@ router.post('/login', (req, res, next) => {
       })
     } else {
       res.json({
-        token: jwt.sign(user.toJSON(), process.env.JWT_SECRET),
-        user
+        token: jwt.sign(user.toJSON(), process.env.JWT_SECRET)
       })
     }
   })(req, res)
