d4be2ebe3b686480866331926f298aa46a748196
devicarus <devicarus@icloud.com>
2022-08-20 00:52:53 +0200

    :heavy_plus_sign: (backend): Replaced bcrypt by bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs/promises')
 
 const passport = require('passport')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const ejs = require('ejs')
 
--- backend/setupPassport.js
+++ backend/setupPassport.js
@@ -3,7 +3,7 @@ const LocalStrategy = require('passport-local')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const User = require('./models/User')
 
